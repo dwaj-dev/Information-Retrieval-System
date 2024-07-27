53b0da8f105da8b386133d0c93a6c37264c29843
Ashish Bharadwaj <its.ashishbharadwaj@gmail.com>
2024-07-27 10:50:12 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.1',
+    author= 'Ashish',
+    author_email= 'its.ashishbharadwaj@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
